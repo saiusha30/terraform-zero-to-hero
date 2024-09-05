490a1661854c6b04bbbe1910c5ca704039620a55
saiusha30 <parvathinausha21@gmail.com>
2024-09-05 10:58:58 +0530

    Update main.tf amiid changed

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-08ee1453725d19cdb"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
